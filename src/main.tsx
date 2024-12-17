7934e1d6c6d575b0566762c872d98f6a55d78f90
alzxdy123 <161685816+alzxdy123@users.noreply.github.com>
2024-12-17 16:55:16 +0800

    first set up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import router from "@/router.tsx";
+import { ThemeProvider } from "./contexts/ThemeProvider";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
---
+++ src/pages/Fincloud.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Fincloud() {
+  return <div>Fincloud</div>;
+}
+
+export default Fincloud;
