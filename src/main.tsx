75f36e5a5f05d456363ea16d7b0600646172508b
alzxdy123 <161685816+alzxdy123@users.noreply.github.com>
2024-12-18 18:48:19 +0800

    fix theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import "./index.css";
-import router from "@/router.tsx";
-import { ThemeProvider } from "./contexts/ThemeProvider";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import './index.css'
+import router from '@/router.tsx'
+// import { ThemeProvider } from "./contexts/ThemeProvider";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        {/* <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme"> */}
+        <RouterProvider router={router} />
+        {/* </ThemeProvider> */}
+    </StrictMode>
+)
