3a5e41ad1beb46bf510b4c25b6f0089aef5c52e5
alzxdy123 <161685816+alzxdy123@users.noreply.github.com>
2024-12-19 01:01:42 +0800

    fix buttton

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -24,7 +24,7 @@ function ContactUs() {
     return (
         <div
             ref={contactRef}
-            className='group fixed bottom-10 right-10 flex cursor-pointer items-center space-x-4 overflow-hidden rounded-full bg-[#1e96d1] px-4 py-4 font-bold text-white'
+            className='group fixed bottom-10 right-10 z-50 flex cursor-pointer items-center space-x-4 overflow-hidden rounded-full bg-[#1e96d1] px-4 py-4 font-bold text-white'
             onMouseEnter={handleMouseEnter}
             onMouseLeave={handleMouseLeave}
         >
