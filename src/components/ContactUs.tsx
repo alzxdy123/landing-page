a1fd6fe84650e172e8de70242bf537fe24029436
alzxdy123 <161685816+alzxdy123@users.noreply.github.com>
2024-12-19 01:06:20 +0800

    try 2

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,9 +2,8 @@ import { MdOutlineMail } from 'react-icons/md'
 
 function ContactUs() {
     return (
-        <div className='group fixed bottom-10 right-10 flex cursor-pointer items-center space-x-4 overflow-hidden rounded-full bg-[#1e96d1] px-4 py-4 font-bold text-white transition-all'>
+        <div className='fixed bottom-10 right-10 flex cursor-pointer items-center rounded-full bg-[#1e96d1] px-4 py-4 font-bold text-white'>
             <MdOutlineMail className='text-2xl font-extrabold' />
-            <div className='hidden group-hover:block'>Contact Us</div>
         </div>
     )
 }
